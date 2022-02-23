8504556416c3c545731e443aee6d84f52098863e
Saty <satymahajan@mac.com>
2022-02-22 19:23:13 -0500

    Refactor and cleanup before TradingView conversion. :)

--- saty_atr_levels.ts
+++ saty_atr_levels.ts
@@ -1,5 +1,5 @@
 # Saty_ATR_Levels
-# v2 By Saty Mahajan (2022)
+# Copyright (C) 2022 Saty Mahajan
 # Author is not responsible for your trading using this script.
 # Data provided in this script is not financial advice.
 #
@@ -10,9 +10,9 @@
 # Features:
 # Shows DTR vs ATR for the day
 # Shows 2 labels:
-# - 23.6% of ATR sweet spot put trigger using previous close
+# - 23.6% of ATR put trigger using previous close
 # and -1 ATR bottom range value using previous close
-# - 23.6% of ATR sweet spot call trigger using previous close
+# - 23.6% of ATR call trigger using previous close
 # and +1 ATR top range value using previous close
 # Configuration:
 # - ATR length
@@ -20,53 +20,52 @@
 # - Use of today's close in order to find levels for tomorrow.
 # - Put trigger cloud
 # - Call trigger cloud
-# - +/- Midrange cloud
+# - +/- Midrange cloud (defaults to 61.8 fib when using default trigger value
 # - +/-1 ATR from previous close white clouds
 
-declare upper;
-
-input ATR_Length = 14;
-input sweet_spot_percentage = 0.236;
+input atr_Length = 14;
+input trigger_percentage = 0.236;
 input use_todays_close = no;
 input close_cloud_on = yes;
-input put_trigger_cloud_on = yes;
-input call_trigger_cloud_on = yes;
-input middle_upper_cloud_on = yes;
-input middle_lower_cloud_on = yes;
-input top_range_cloud_on = yes;
-input bottom_range_cloud_on = yes;
+input lower_trigger_cloud_on = yes;
+input lower_middle_cloud_on = yes;
+input lower_atr_cloud_on = yes;
+input upper_trigger_cloud_on = yes;
+input upper_middle_cloud_on = yes;
+input upper_atr_cloud_on = yes;
 
-def prev_close = if use_todays_close then close(period = AggregationPeriod.DAY) else close(period = AggregationPeriod.DAY)[1];
-def atr = WildersAverage(TrueRange(high(period = AggregationPeriod.DAY), close(period = AggregationPeriod.DAY), low(period = AggregationPeriod.DAY)), ATR_Length);
+def previous_close = if use_todays_close then close(period = AggregationPeriod.DAY) else close(period = AggregationPeriod.DAY)[1];
+def atr = WildersAverage(TrueRange(high(period = AggregationPeriod.DAY), close(period = AggregationPeriod.DAY), low(period = AggregationPeriod.DAY)), atr_Length);
 def todays_high = Highest(high(period = AggregationPeriod.DAY), 1);
 def todays_low = Lowest(low(period = AggregationPeriod.DAY), 1);
 def dtr = todays_high - todays_low;
-def dtr_percent = Round((dtr / atr) * 100, 0);
-def pt = prev_close - (sweet_spot_percentage * atr);
-def ct = prev_close + (sweet_spot_percentage * atr);
-def br = prev_close - atr;
-def tr = prev_close + atr;
-def mbt = (pt + br) / 2;
-def mtt = (ct + tr) / 2;
+def dtr_percent_of_atr = Round((dtr / atr) * 100, 0);
+def lower_trigger = previous_close - (trigger_percentage * atr);
+def upper_trigger = previous_close + (trigger_percentage * atr);
+def lower_atr = previous_close - atr;
+def upper_atr = previous_close + atr;
+def lower_middle = (lower_trigger + lower_atr) / 2;
+def upper_middle = (upper_trigger + upper_atr) / 2;
 
 # Labels
-AddLabel (yes, "DTR ($" + Round (dtr , 2) + ") is " + Round (dtr_percent, 0) + "% of ATR ($" + Round (atr, 2) + ")   " , (if dtr_percent <= 70 then Color.GREEN else if dtr_percent >= 90 then Color.RED else Color.ORANGE));
-AddLabel (yes, "Puts < $" + Round (pt, 2) + " | -1 ATR: $" +  Round (br, 2) + "   ", Color.ORANGE);
-AddLabel (yes, "Calls > $" + Round (ct, 2) + " | +1 ATR: $" + Round (tr, 2) + "   ", Color.CYAN);
+AddLabel (yes, "DTR ($" + Round (dtr , 2) + ") is " + Round (dtr_percent_of_atr, 0) + "% of ATR ($" + Round (atr, 2) + ")   " , (if dtr_percent_of_atr <= 70 then Color.GREEN else if dtr_percent_of_atr >= 90 then Color.RED else Color.ORANGE));
+AddLabel (yes, "Puts < $" + Round (lower_trigger, 2) + " | -1 ATR: $" +  Round (lower_atr, 2) + "   ", Color.ORANGE);
+AddLabel (yes, "Calls > $" + Round (upper_trigger, 2) + " | +1 ATR: $" + Round (upper_atr, 2) + "   ", Color.CYAN);
 
 def cloud_size = 0.01;
 
 # Previous Close cloud
-AddCloud(if close_cloud_on then (prev_close - (atr * cloud_size / 2)) else Double.NaN, (prev_close + (atr * cloud_size / 2)), Color.WHITE, Color.WHITE);
+AddCloud(if close_cloud_on then (previous_close - (atr * cloud_size / 2)) else Double.NaN, (previous_close + (atr * cloud_size / 2)), Color.WHITE, Color.WHITE);
 
-# Put / Call trigger clouds
-AddCloud(if put_trigger_cloud_on then pt else Double.NaN, (pt - (atr * cloud_size)), Color.ORANGE, Color.ORANGE);
-AddCloud(if call_trigger_cloud_on then ct else Double.NaN, (ct + (atr * cloud_size)), Color.CYAN, Color.CYAN); 
+# Trigger clouds
+AddCloud(if lower_trigger_cloud_on then lower_trigger else Double.NaN, (lower_trigger - (atr * cloud_size)), Color.ORANGE, Color.ORANGE);
+AddCloud(if upper_trigger_cloud_on then upper_trigger else Double.NaN, (upper_trigger + (atr * cloud_size)), Color.CYAN, Color.CYAN); 
 
-# Middle range  clouds
-AddCloud(if middle_lower_cloud_on then mbt else double.nan, (mbt - (atr * cloud_size)), Color.GRAY, Color.GRAY);
-AddCloud(if middle_upper_cloud_on then mtt else double.nan, (mtt + (atr * cloud_size)), Color.GRAY, Color.GRAY); 
+# Middle clouds
+AddCloud(if lower_middle_cloud_on then lower_middle else double.nan, (lower_middle - (atr * cloud_size)), Color.GRAY, Color.GRAY);
+AddCloud(if upper_middle_cloud_on then upper_middle else double.nan, (upper_middle + (atr * cloud_size)), Color.GRAY, Color.GRAY); 
 
 # +/- 1 ATR clouds
-AddCloud(if bottom_range_cloud_on then br else Double.NaN, (br - (atr * cloud_size)), Color.WHITE, Color.WHITE);
-AddCloud(if top_range_cloud_on then tr else Double.NaN, (tr + (atr * cloud_size)), Color.WHITE, Color.WHITE);
+AddCloud(if lower_atr_cloud_on then lower_atr else Double.NaN, (lower_atr - (atr * cloud_size)), Color.WHITE, Color.WHITE);
+AddCloud(if upper_atr_cloud_on then upper_atr else Double.NaN, (upper_atr + (atr * cloud_size)), Color.WHITE, Color.WHITE);
+ 
